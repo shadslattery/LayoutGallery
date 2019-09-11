c029c4685ceaa18987229a41c6c7aa0dc3de710c
shadslattery <shadslattery@gmail.com>
2019-09-11 13:56:32 -0400

    Link Everything together

---
+++ app/src/main/java/com/example/layoutgallery/LinkEverythingtogether.java
@@ -0,0 +1,14 @@
+package com.example.layoutgallery;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinkEverythingtogether extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
